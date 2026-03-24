9d5fb340e40c3f5c20639883b7e5c4a50076adac
Najehnator <kingnajeh12321@gmail.com>
2026-03-24 16:26:13 +0100

    Call add(a, b) and print result

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def run():
    print("1) Add\n2) Subtract\n3) Mulitply\n4 Divide\n0 Exit")
    choice = input("Your choice: ")
-   if choice == "1": print("TODO Add")
+   if choice == "1": print(add(a, b))
    a = float(input("a: "))
    b = float(input("b: "))
